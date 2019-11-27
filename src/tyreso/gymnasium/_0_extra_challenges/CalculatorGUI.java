880aa9594e968ce559a67a2f2e06100410687861
Sesa0615 <lolfrag45@gmail.com>
2019-11-27 14:40:33 +0100

    441dad

---
+++ src/tyreso/gymnasium/_0_extra_challenges/CalculatorGUI.java
@@ -0,0 +1,20 @@
+package tyreso.gymnasium._0_extra_challenges;
+
+import javax.swing.*;
+
+public class CalculatorGUI {
+
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button5;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
